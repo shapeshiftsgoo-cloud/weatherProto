c5fdb6c279f456ab52cfca900dc14c1c87e11630
Yaren Globus <Damascenoleonan@gmail.com>
2026-05-03 20:33:55 -0300

    Initial commit

---
+++ src/main/java/com/weatheringu/weatherProto/WeatherProtoApplication.java
@@ -0,0 +1,13 @@
+package com.weatheringu.weatherProto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherProtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weatheringu/weatherProto/WeatherProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatheringu.weatherProto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherProtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
